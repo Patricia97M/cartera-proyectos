7faf72b686283ed2fb6ee4e8685351ff25de08c0
Patricia Monares <patricia.monares@openagents.io>
2025-12-16 23:59:21 -0300

    fix: ajuste en redireccionamiento

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -12,7 +12,7 @@ const Login: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     await login(email, password);
-    if (authState.error !== null) {
+    if (!authState.error) {
       navigate("/projects");
     }
   };
